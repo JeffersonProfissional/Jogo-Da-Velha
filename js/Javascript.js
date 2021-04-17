0707429ecbc58169641a25ed9caaca33b60d6285
Jefferson <kio123457@gmail.com>
2021-04-16 21:00:31 -0300

    Erro corrigido, finalização do Jogo da Velha-v1

--- js/Javascript.js
+++ js/Javascript.js
@@ -2,15 +2,24 @@ let x = document.querySelector(".x");
 let o = document.querySelector(".o");
 let boxes = document.querySelectorAll(".box");
 let buttons = document.querySelectorAll("#buttons-container button");
+let buttonsCB = document.querySelectorAll("#buttons-container-2 button");
 let messageContainer = document.querySelector("#message");
 let messageText = document.querySelector("#message p");
 let container = document.querySelector("#container");
+let scoreboardX = document.querySelector("#scoreboard-1");
+let scoreboardO = document.querySelector("#scoreboard-2");
+let buttonContinue = document.querySelector("#continue-play");
+let buttonBack = document.querySelector("#back-play");
 let secondPlayer;
+let continueOrBack;
 
 //Contador de jogadas
 
 let player1 = 0;
 let player2 = 0;
+
+//Selecionar player vs player ou player vs CPU
+EventButtons();
 
 // adcionando o evento de click aos boxes
 
@@ -36,10 +45,9 @@ for(let i = 0; i < boxes.length; i++) {
 
 				if(secondPlayer == 'ia-player'){
 					player2++;
-					//funcao executar jogar
+					//Executa a jogada do CPU
 					ComputerPlay();
-
-					
+				
 				}
 			}else{
 				player2++;
@@ -52,33 +60,31 @@ for(let i = 0; i < boxes.length; i++) {
 }
 
 //Evento dos botões 2 players e IA
-for(let i = 0; i < buttons.length; i++){
 
-	buttons[i].addEventListener("click", () => {
+function EventButtons() {
 
-		secondPlayer = buttons[i].getAttribute("id");
+	for(let i = 0; i < buttons.length; i++){
 
-		for(let j = 0; j < buttons.length; j++){
-			buttons[j].style.display = 'none';
-		}
+		buttons[i].addEventListener("click", () => {
 
-		setTimeout(() =>{
+			secondPlayer = buttons[i].getAttribute("id");
 
-			container.classList.remove("hide");
+			for(let j = 0; j < buttons.length; j++){
+				buttons[j].classList.add("hide");
 
-		}, 1000);
+			}
 
-		/*switch (secondPlayer){
-			case 'two-players':
-				
-				break;
-			case 'ia-player':
-				
-				break;
-		}*/
-		
-	});
+			setTimeout(() =>{
+
+				container.classList.remove("hide");
+
+			}, 1000);
+			
+		});
+	}
 }
+
+//Check elemento pra clonar e enviar para o evento de clique do jogador
 
 function CheckElement(player1, player2){
 	if(player1 == player2) {
@@ -108,7 +114,6 @@ function CheckWinCondition() {
 	let b9 = document.getElementById("block-9");
 	let b1ClassChild,b2ClassChild,b3ClassChild,result,player;
 
-
 	if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0){
 
 		b1ClassChild = b1.childNodes[0].className;
@@ -121,7 +126,6 @@ function CheckWinCondition() {
 		if(result){
 			DeclareWinner(player);
 		}
-
 
 	}if(b4.childNodes.length > 0 && b5.childNodes.length > 0 && b6.childNodes.length > 0){
 
@@ -136,7 +140,6 @@ function CheckWinCondition() {
 			DeclareWinner(player);
 		}
 
-
 	}if(b7.childNodes.length > 0 && b8.childNodes.length > 0 && b9.childNodes.length > 0){
 
 		b1ClassChild = b7.childNodes[0].className;
@@ -149,7 +152,6 @@ function CheckWinCondition() {
 		if(result){
 			DeclareWinner(player);
 		}
-
 
 	}if(b1.childNodes.length > 0 && b4.childNodes.length > 0 && b7.childNodes.length > 0){
 
@@ -164,7 +166,6 @@ function CheckWinCondition() {
 			DeclareWinner(player);
 		}
 
-
 	}if(b2.childNodes.length > 0 && b5.childNodes.length > 0 && b8.childNodes.length > 0){
 
 		b1ClassChild = b2.childNodes[0].className;
@@ -177,7 +178,6 @@ function CheckWinCondition() {
 		if(result){
 			DeclareWinner(player);
 		}
-
 
 	}if(b3.childNodes.length > 0 && b6.childNodes.length > 0 && b9.childNodes.length > 0){
 
@@ -192,7 +192,6 @@ function CheckWinCondition() {
 			DeclareWinner(player);
 		}
 
-
 	}if(b1.childNodes.length > 0 && b5.childNodes.length > 0 && b9.childNodes.length > 0){
 
 		b1ClassChild = b1.childNodes[0].className;
@@ -205,7 +204,6 @@ function CheckWinCondition() {
 		if(result){
 			DeclareWinner(player);
 		}
-
 
 	}if(b3.childNodes.length > 0 && b5.childNodes.length > 0 && b7.childNodes.length > 0){
 
@@ -234,7 +232,6 @@ function CheckWinCondition() {
 	}
 }
 	
-
 //Checa se uma linha está preenchida com elementos iguais
 function CheckLine(block1, block2, block3){
 
@@ -248,17 +245,17 @@ function CheckLine(block1, block2, block3){
 		}
 }
 
-
 //Limpa o jogo, declara o vencedor e atualiza o placar
 function DeclareWinner(winner){
 
-	let scoreboardX = document.querySelector("#scoreboard-1");
-	let scoreboardO = document.querySelector("#scoreboard-2");
 	let msg = '';
 
 	if(winner == 'x') {
 		scoreboardX.textContent = parseInt(scoreboardX.textContent) + 1;
 		msg = "jogador X venceu !";
+	}else if(winner == 'o' && secondPlayer == 'ia-player'){
+		scoreboardO.textContent = parseInt(scoreboardO.textContent) + 1;
+		msg = "CPU é o vencedor !";
 	}else if (winner == 'o'){
 		scoreboardO.textContent = parseInt(scoreboardO.textContent) + 1;
 		msg = "jogador O venceu !";
@@ -275,6 +272,10 @@ function DeclareWinner(winner){
 		messageContainer.classList.add("hide");
 	},4000);
 
+	//Continuar ou voltar
+
+	ContinueOrBackPlay();
+
 	//Zerar as jogadas
 
 	player1 = 0;
@@ -288,47 +289,79 @@ function DeclareWinner(winner){
 	}
 }
 
-//Função com ERRO
 //Computa a jogada da Inteligencia Artificial
 function ComputerPlay(){
 
 	let cloneO = o.cloneNode(true);
 	counter = 0;
 	filled = 0;
-	
-	//let randomNumber = Math.floor(Math.random() * 5);
-
-	//console.log(counter);
-	//console.log(filled);
 
 	for(let i = 0; i < boxes.length; i++){
 
-		let randomNumber = Math.floor(Math.random() * 9);
+		let randomNumber = Math.floor(Math.random() * 5);
 
-		if(boxes[i].childNodes[0] == undefined && i == randomNumber){
-
+		if(boxes[i].childNodes[0] == undefined){
+			if(randomNumber <= 1){
 				boxes[i].appendChild(cloneO);
 				counter++;
 				break;
-			}else {
-				filled++;
 			}
-
-			if(counter == 0 && filled < 9){
-				ComputerPlay();
-			}
-		/*	boxes[i].appendChild(cloneO);
-			counter++;
-			break;
-		}else{
-			filled += 1; 
+		}else {
+			filled++;
 		}
-
-		if(filled < 9 && counter == 0){
-			filled = 0;
-			console.log("Contei 8");
-			ComputerPlay();
-		}*/
-
 	}
+
+	if(counter == 0 && filled < 9){
+		ComputerPlay();
+	}
+}
+
+//Continuar Player vs Player ou Player vs CPU 
+
+function ContinueOrBackPlay() {
+
+container.classList.add("hide");
+
+	for(let i = 0; i < buttons.length; i++){
+
+		buttonsCB[i].addEventListener("click", () => {
+
+			if(buttonsCB[i].getAttribute("id") == "continue-play"){
+				hideClassCB(buttonContinue, buttonBack);
+				container.classList.remove("hide");
+
+			}else if(buttonsCB[i].getAttribute("id") == "back-play"){
+				hideClassCB(buttonContinue, buttonBack);
+
+				showButtons(buttons[0],buttons[1]);
+
+				scoreboardO.textContent = 0;
+				scoreboardX.textContent = 0;
+				EventButtons();
+			}
+
+		});
+	}
+}
+
+function showClassCB(Continue, Back){
+	Continue.classList.remove("hide");
+	Back.classList.remove("hide");
+
+}
+
+function hideClassCB(Continue,Back){
+
+	Continue.classList.add("hide");
+	Back.classList.add("hide");
+
+}
+
+function showButtons(button1, button2){
+
+	console.log(button1, button2);
+
+	button1.classList.remove("hide");
+	button2.classList.remove("hide");
+
 }
